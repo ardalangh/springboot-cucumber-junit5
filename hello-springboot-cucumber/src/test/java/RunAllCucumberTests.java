55bf943c3a65658b13a091a0afa4e69dba6d709d
Ardy Ghoorchian <ardalan.ghoorchian@gm.com>
2023-03-30 15:16:12 -0400

    [Ardy]: Changed the name

--- hello-springboot-cucumber/src/test/java/RunAllCucumberTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@Suite
-@SelectClasspathResource("bdd")
-public class RunAllCucumberTests {
-}
--- hello-springboot-cucumber/src/test/resources/bdd/F2.feature
+++
@@ -1,8 +0,0 @@
-Feature: F2
-  Scenario Outline: An invalid module code does not return properties 2
-    Given Service code is not "<ServiceCode>"
-    Then do not return properties
-    Examples:
-      | ServiceCode |
-      | BAD        |
-
